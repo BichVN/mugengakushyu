7a89cf5aa945484075e73bdad22e32dac5e04322
bichvn <tieuke107@gmail.com>
2015-10-19 06:04:29 +0000

    addUserLogs

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :user_logs
+  resources :user_logs
+  resources :user_logs
+  root 'static_pages#home'
+
   devise_for :users
   resources :books
   # The priority is based upon order of creation: first created -> highest priority.
